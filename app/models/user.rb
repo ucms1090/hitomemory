14d6860984cf2aafbfbcd85df8f4e9e7b9130087
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2023-01-17 23:48:04 +0000

    fix]ユーザーとメモリーのログインキーを変更。devise.rbの修正による

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  devise :database_authenticatable, authentication_keys: [:email]
+
   has_one_attached :user_image
 
 end
