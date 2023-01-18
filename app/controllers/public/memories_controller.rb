147652c523b6d5f710bdd99f0883f66e0cfb73b4
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2023-01-18 00:27:06 +0000

    add]カレンダー表示追加

--- app/controllers/public/memories_controller.rb
+++ app/controllers/public/memories_controller.rb
@@ -1,13 +1,46 @@
 class Public::MemoriesController < ApplicationController
+
   def index
+    @memories = Memory.all
+    @memory = Memory.new
   end
 
   def new
+    @memory = Memory.new
   end
 
   def edit
+    @memory = Memory.find(params[:id])
   end
 
   def show
+    @memory = Memory.find(params[:id])
   end
+
+  def create
+    Memory.create(memory_params)
+    redirect_to memory_path(Memory.id)
+  end
+
+  def update
+    @memory = Memory.find(params[:id])
+    if @memory.update(memory_params)
+      redirect_to memory_path(@memory.id), notice: "success"
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @memory = Memory.find(params[:id])
+    @memory.destroy
+    redirect_to memory_root_path
+  end
+
+  private
+
+  def memory_params
+    params.require(:memory).permit(:tag_id, :title, :content, :start_time)
+  end
+
 end
