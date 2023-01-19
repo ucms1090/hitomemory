e63d1a56b0a7f40d121847339fc17604ea976bc6
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2023-01-19 03:23:04 +0000

    fix]メモリーログアウト時のリダイレクト先をユーザーページへ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :memory_name])
   end
 
+  def after_sign_out_path_for(resource)
+    if resource == :memory
+      user_root_path
+    elsif resource == :user
+      root_path
+    end
+  end
+
 end
